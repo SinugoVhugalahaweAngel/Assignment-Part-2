c3ad4f368c9ef8404a24dffb82184f0a22fc53d6
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-18 19:48:14 +0000

    Import Scanner

---
+++ src/main/java/com/mycompany/useraccount/UserAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.useraccount;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class UserAccount {
+
+    public static void main(String[] args) {
+        
+    }
+}
